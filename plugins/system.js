2835209c0f188dcf5ddbc365d6ee86f1ed5216db
ayesh406 <ayeshboys1988@gmail.com>
2025-05-10 15:20:16 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,18 +12,21 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `╭━━〔 *𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃* 〕━━┈⊷
+let status = `                   
+╭━━〔 *𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃 🥷🇱🇰* 〕━━┈⊷
 ┃✯╭─────────────·๏
-┃✯┃• *⏳ 𝗨𝗽𝘁𝗶𝗺𝗲:-* ${runtime(process.uptime())}
-┃✯┃• *📟 𝗥𝗮𝗺:-* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
-┃✯┃• *⚙️ 𝗣𝗹𝗮𝘁𝗳𝗼𝗿:-* ${os.hostname()}
-┃✯┃• *👨‍💻 𝗢𝘄𝗻𝗲𝗿:-* 𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃 🥷
-┃✯│• *🥷 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 𝗻𝗯:-* 94760746437
+┃✯┃• *⏳Uptime*: ${runtime(process.uptime())}
+┃✯┃• *📟 Ram*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
+┃✯┃• *⚙️ Platform:-* ${os.hostname()}
+┃✯┃• *👨‍💻 Owner*: 𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃 🥷
+┃✯┃• *🥷 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 𝗻𝗯:-* 94760746437
 ┃✯└───────────┈⊷
 ╰──────────────┈⊷
 
-> *𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃 🥷🇱🇰*
+> 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝐀𝐒𝐇𝐈𝐘𝐀_𝐌𝐃 🥷💗
 `
+
+    
 return reply(`${status}`)
   
 }catch(e){
