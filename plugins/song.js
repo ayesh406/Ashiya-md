57edb65419d7b32db82399d87cef0c2ab7b3a92b
ayesh406 <ayeshboys1988@gmail.com>
2025-05-17 18:38:42 +0530

    Update and rename song2.js to song.js

--- plugins/song.js
+++ plugins/song.js
@@ -3,7 +3,7 @@ const yts = require('yt-search');
 const axios = require("axios");
 
 cmd({
-    pattern: "song2",
+    pattern: "song",
     alias: "play",
     desc: "To download songs.",
     react: "🎵",
